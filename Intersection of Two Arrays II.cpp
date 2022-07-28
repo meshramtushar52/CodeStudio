412b640665bd6db53d6182ecc1d0bc9b5f5375e4
Tushar Meshram <109428259+meshramtushar52@users.noreply.github.com>
2022-07-29 01:11:42 +0530

    Create Intersection of Two Arrays II.cpp

---
+++ Intersection of Two Arrays II.cpp
@@ -0,0 +1,19 @@
+void intersection(int *input1, int *input2, int size1, int size2)
+{
+    int i ,j;
+    
+    for( int i = 0; i < size1; i++){
+        
+        for ( int j = 0; j < size2; j++){
+            
+            if (input1[i] == input2[j]){
+                cout << input1[i] <<" ";
+                input2[j] = -1;
+                break;
+            }
+            else if(input1[i] != input2[j]){
+                continue;
+            }
+        }
+    }
+}
